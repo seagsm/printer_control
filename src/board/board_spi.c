38b20f6b2c5ecedfefbf633e37be1d3ef4a6d2a7
Vadym Volokitin Office <seagsm_trash@mail.ru>
2014-09-10 20:08:05 +0200

    Added some function spi.

--- src/board/board_spi.c
+++ src/board/board_spi.c
@@ -9,18 +9,23 @@ BOARD_ERROR be_board_spi_init(void)
 
     be_result |= be_board_spi_a_gpio_init();
     be_result |= be_board_spi_a_module_init();
+    
+    be_result |= be_board_spi_b_gpio_init();
+    be_result |= be_board_spi_b_module_init();
 
+    
+    
     return(be_result);
 }
 
-/* This function init SPI A interface. */
+/* This function init SPI A interface. Master. */
 static BOARD_ERROR be_board_spi_a_gpio_init(void)
 {
     BOARD_ERROR be_result = BOARD_ERR_OK;
 
-    be_result |= be_board_pin_init( BOARD_SPI_A_MOSI_PORT,    BOARD_SPI_A_MOSI_PIN,     GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);
-    be_result |= be_board_pin_init( BOARD_SPI_A_MISO_PORT,    BOARD_SPI_A_MISO_PIN,     GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);
-    be_result |= be_board_pin_init( BOARD_SPI_A_SCK_PORT,     BOARD_SPI_A_SCK_PIN,      GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);
+    be_result |= be_board_pin_init( GPIOA,      GPIO_Pin_7,     GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);           /* MOSI_PIN */
+    be_result |= be_board_pin_init( GPIOA,      GPIO_Pin_6,     GPIO_Speed_10MHz,   GPIO_Mode_IN_FLOATING);     /* MISO_PIN */
+    be_result |= be_board_pin_init( GPIOA,      GPIO_Pin_5,     GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);           /* SCK_PIN */
 
     return(be_result);
 }
@@ -30,29 +35,75 @@ static BOARD_ERROR be_board_spi_a_module_init(void)
 {
     BOARD_ERROR be_result = BOARD_ERR_OK;
 
-    SPI_InitTypeDef SPIConf;
-    /* Just TX. */
-    SPIConf.SPI_Direction = SPI_Direction_1Line_Tx;
-
-    /* Set Master mode. */
-    SPIConf.SPI_Mode = SPI_Mode_Master;
-    /* Set 8 bit send. */
-    SPIConf.SPI_DataSize = SPI_DataSize_8b;
-    /* Set mode 00.*/
-    SPIConf.SPI_CPOL = SPI_CPOL_Low;
-    SPIConf.SPI_CPHA = SPI_CPHA_1Edge;
-    SPIConf.SPI_NSS = SPI_NSS_Soft;
-    /* Set TX speed. */
-    SPIConf.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
-    /* Set MSB. */
-    SPIConf.SPI_FirstBit = SPI_FirstBit_MSB;
-    /* Call init function. */
-    SPI_Init(SPI1, &SPIConf);
+    SPI_InitTypeDef SPI_InitStructure;
+   
+    /* SPI1 configuration */
+    SPI_InitStructure.SPI_Direction     = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_Mode          = SPI_Mode_Master;
+    SPI_InitStructure.SPI_DataSize      = SPI_DataSize_16b;
+    SPI_InitStructure.SPI_CPOL          = SPI_CPOL_High;
+    SPI_InitStructure.SPI_CPHA          = SPI_CPHA_2Edge;
+    SPI_InitStructure.SPI_NSS           = SPI_NSS_Soft;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+    SPI_InitStructure.SPI_FirstBit      = SPI_FirstBit_MSB;
+    SPI_InitStructure.SPI_CRCPolynomial = 7U;
+    
+    SPI_Init(SPI1, &SPI_InitStructure);
+ 
     /* Turn On SPI A */
     SPI_Cmd(SPI1, ENABLE);
+    
     /* SS = 1 */
     SPI_NSSInternalSoftwareConfig(SPI1, SPI_NSSInternalSoft_Set);
 
     return(be_result);
 }
+
+
+
+/* This function init SPI B interface. */
+static BOARD_ERROR be_board_spi_b_gpio_init(void)
+{
+    BOARD_ERROR be_result = BOARD_ERR_OK;
+
+    be_result |= be_board_pin_init( GPIOB,      GPIO_Pin_15,     GPIO_Speed_10MHz,   GPIO_Mode_IN_FLOATING);    /* MOSI_PIN */
+    be_result |= be_board_pin_init( GPIOB,      GPIO_Pin_14,     GPIO_Speed_10MHz,   GPIO_Mode_AF_PP);          /* MISO_PIN */
+    be_result |= be_board_pin_init( GPIOB,      GPIO_Pin_13,     GPIO_Speed_10MHz,   GPIO_Mode_IN_FLOATING);    /* SCK_PIN */
+
+    return(be_result);
+}
+
+/* This function init SPI b module. */
+static BOARD_ERROR be_board_spi_b_module_init(void)
+{
+    BOARD_ERROR be_result = BOARD_ERR_OK;
+
+    SPI_InitTypeDef SPI_InitStructure;
+    
+    SPI_InitStructure.SPI_Direction     = SPI_Direction_2Lines_FullDuplex;
+    SPI_InitStructure.SPI_Mode          = SPI_Mode_Slave;
+    SPI_InitStructure.SPI_DataSize      = SPI_DataSize_16b;
+    SPI_InitStructure.SPI_CPOL          = SPI_CPOL_High;                     /* SCK idle high */
+    SPI_InitStructure.SPI_CPHA          = SPI_CPHA_2Edge;                    /* second transition -> SCK Idle high => capture on rising edge of clock */
+    SPI_InitStructure.SPI_NSS           = SPI_NSS_Hard;
+    SPI_InitStructure.SPI_FirstBit      = SPI_FirstBit_MSB;
+    
+    SPI_Init(SPI2, &SPI_InitStructure);
+
+    return(be_result);
+}
+
+
+
+
+void   SPI1_IRQHandler(void)
+{
+ /* SPI_I2S_GetITStatus(SPI_TypeDef* SPIx, uint8_t SPI_I2S_IT) */
+}
+void   SPI2_IRQHandler(void)
+{
+
+}
+
+
 
--- src/board/board_spi.h
+++ src/board/board_spi.h
@@ -8,9 +8,19 @@
 #include "board_gpio.h"
 #include "board_sys_tick.h"
 
-
+/* SPI_A pin definition. */
 #define BOARD_SPI_A_SCK_PIN     GPIO_Pin_5
-#define BOARD_SPI_A_SCK_PORT  GPIOA
+#define BOARD_SPI_A_SCK_PORT    GPIOA
+
+#define BOARD_SPI_A_MISO_PIN    GPIO_Pin_6
+#define BOARD_SPI_A_MISO_PORT   GPIOA
+
+#define BOARD_SPI_A_MOSI_PIN    GPIO_Pin_7
+#define BOARD_SPI_A_MOSI_PORT   GPIOA
+
+/* SPI_B pin definition. */
+#define BOARD_SPI_A_SCK_PIN     GPIO_Pin_5
+#define BOARD_SPI_A_SCK_PORT    GPIOA
 
 #define BOARD_SPI_A_MISO_PIN    GPIO_Pin_6
 #define BOARD_SPI_A_MISO_PORT   GPIOA
@@ -21,9 +31,19 @@
 
 
 
+
+
+
        BOARD_ERROR be_board_spi_init(void);
 static BOARD_ERROR be_board_spi_a_gpio_init(void);
 static BOARD_ERROR be_board_spi_a_module_init(void);
+
+static BOARD_ERROR be_board_spi_b_gpio_init(void);
+static BOARD_ERROR be_board_spi_b_module_init(void);
+
+void   SPI1_IRQHandler(void);
+void   SPI2_IRQHandler(void);
+
 
 
 #endif
